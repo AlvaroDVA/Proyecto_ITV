b53f4427aac1c2fa60d16d042aa934cdc9a93e8d
Mario Bueno <maarioo2525@gmail.com>
2023-05-19 09:05:52 +0200

    Index html, index xss, index js, header y footer terminados

---
+++ HTML/Home/JavaScriptFotos.js
@@ -0,0 +1,48 @@
+var imagenes = [
+    "Imagenes/Imagen-Uno.jpg",
+    "Imagenes/Imagen-Dos.jpg",
+    "Imagenes/Imagen-Tres.jpg"
+  ];
+  
+  document.getElementById("Imagen").src = imagenes[0];
+  
+  var SliderDerecha = document.querySelector(".slider-derecho");
+  var SliderIzquierda = document.querySelector(".slider-izquierdo");
+  var Contador = 0;
+  var Intervalo = setInterval(MoverDerecha, 3500);
+  
+  var servicesBar = document.getElementById("servicesBar");
+  
+  function MoverDerecha() {
+    Contador++;
+    if (Contador > imagenes.length - 1) {
+      Contador = 0;
+    }
+    document.getElementById("Imagen").src = imagenes[Contador];
+  }
+  
+  SliderDerecha.addEventListener("click", function() {
+    clearInterval(Intervalo);
+    MoverDerecha();
+    Intervalo = setInterval(MoverDerecha, 3500);
+  });
+  
+  function MoverIzquierda() {
+    Contador--;
+    if (Contador < 0) {
+      Contador = imagenes.length - 1;
+    }
+    document.getElementById("Imagen").src = imagenes[Contador];
+  }
+  
+  SliderIzquierda.addEventListener("click", function() {
+    clearInterval(Intervalo);
+    MoverIzquierda();
+    Intervalo = setInterval(MoverDerecha, 3500);
+  });
+  
+  function toggleServices() {
+    servicesBar.classList.toggle("visible");
+  }
+  
+  
