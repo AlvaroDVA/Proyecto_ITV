6171fd29b060a69922823dc9888f4917de0bffc9
Mario Bueno <maarioo2525@gmail.com>
2023-05-22 14:18:33 +0200

    LM-Web-V1.1.0
    Cambios que nos pidió la profe realizados

--- HTML/src/js/form.js
+++ HTML/src/js/form.js
@@ -11,10 +11,16 @@ function validaciones() {
     if (validarEmail() == false) {
         return
     }
-    if (validarNumero() == false) {
+    if (validarTelefono() == false) {
         return
     }
     if (validarMatricula() == false) {
+        return
+    }
+    if (validarDNI() == false) {
+        return
+    }
+    if(validarMarca() == false) {
         return
     }
     enviarCorreo()
@@ -68,13 +74,36 @@ function validarEmail() {
     }
 }
 
-function validarNumero() {
+function validarTelefono() {
     let numero = document.getElementById("telefono")
     if (isNaN(parseInt(numero.value)) && numero.value.length != 9) {
         alert("No has introducido un numero valido")
         return false
     }
     return true
+}
+
+function validarDNI() {
+    let DNI = document.getElementById("dni")
+    let regex = /^[0-9]{8}[A-Z]{1}$/;
+
+    if (regex.test(DNI.value)) {
+        return true
+    } else {
+        alert("No has introducido un DNI válido")
+        return false
+    }
+}
+
+function validarMarca() {
+    let marca = document.getElementById("marca")
+    let regex = /^[^\d\s]+$/;
+    if (regex.test(marca.value)) {
+        return true
+    } else {
+        alert("La marca del vehículo no puede contener digitos")
+        return false
+    }
 }
 
 function validarMatricula() {
@@ -90,15 +119,22 @@ function validarMatricula() {
 }
 
 function enviarCorreo() {
-    const email = document.getElementById("email").value;
+    
     const nombre = document.getElementById("nombre").value;
     const apellido = document.getElementById("apellido").value;
-    const matricula = document.getElementById("matricula").value;
-    const numero = document.getElementById("telefono").value;
+    const email = document.getElementById("email").value;
+    const telefono = document.getElementById("telefono").value;
+    const DNI = document.getElementById("dni").value;
     const vehiculo = document.getElementById("vehiculo").value;
+    const marca = document.getElementById("marca").value;
+    const modelo = document.getElementById("modelo").value;
+    const motor = document.getElementById("motor").value;
+    const matricula = document.getElementById("matricula").value;
+    const matriculacion = document.getElementById("año-matriculacion").value;
+    const revision = document.getElementById("ultima-revision").value;
 
     const subject = "Datos del formulario";
-    const body = `Nombre: ${nombre}\nApellido: ${apellido}\nEmail: ${email}\nMatrícula: ${matricula}\nNúmero: ${numero}\nVehículo: ${vehiculo}`;
+    const body = `Nombre: ${nombre}\nApellido: ${apellido}\nEmail: ${email}\nNúmero de Teléfono: ${telefono}\nDNI: ${DNI}\nTipo de Vehículo: ${vehiculo}\nMarca: ${marca}\nModelo: ${modelo}\nMotor: ${motor}\nMatrícula: ${matricula}\nAño de matriculación: ${matriculacion}\nÚltima revisión: ${revision}`;
 
     const mailtoLink = `mailto:mohamad.elsayed@alumno.iesluisvives.org?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 
@@ -107,4 +143,23 @@ function enviarCorreo() {
 
 function toggleServices() {
     servicesBar.classList.toggle("visible");
+}
+
+
+
+const formulario = document.getElementById("miFormulario");
+const botonEnviar = document.getElementById("boton");
+
+formulario.addEventListener("input", validarFormulario);
+
+function validarFormulario() {
+  const camposCompletos = Array.from(formulario.elements).every((elemento) => {
+    return elemento.value !== "";
+  });
+
+  if (camposCompletos) {
+    botonEnviar.removeAttribute("disabled");
+  } else {
+    botonEnviar.setAttribute("disabled", "disabled");
+  }
 }
